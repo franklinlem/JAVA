60abc147935a3cb120d6b724a8288319a9783987
Franklin Martins <franklinlem@yahoo.com>
2023-10-01 16:38:00 -0300

    estudando java

---
+++ SystemIn/src/systemin/SystemIn.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package systemin;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author franklin
+ */
+public class SystemIn {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o nome do aluno: ");
+        String nome = teclado.nextLine();
+        System.out.println("Digite a nota do aluno: ");
+        float nota = teclado.nextFloat();
+        System.out.format("A nota de %s é %.1f \n", nome, nota);
+    }
+    
+}
--- TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -1,22 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tiposprimitivos;
 
-/**
- *
- * @author franklin
- */
+import java.util.Scanner;
+
 public class TiposPrimitivos {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         float nota = 8.3f;
+        //digite sout e tab para aparecer o comando completo de print
         System.out.println("A nota é " + nota);
+        System.out.printf("A nota é %.4f \n", nota);
+        System.out.format("Sua nota é %.2f \n", nota);
+        
     }
     
 }
