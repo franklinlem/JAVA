50eeb8313fed7f68b70bbf47624cabdcedc38bf3
Franklin Martins <franklinlem@yahoo.com>
2023-10-16 17:00:34 -0300

    estudando java

---
+++ AulaPOO/src/aulapoo/Caneta.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aulapoo;
+
+/**
+ *
+ * @author Polícia Civil
+ */
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    
+    void status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+    void rabiscar(){
+        
+    }
+    void tampar(){
+        
+    }
+    void destampar(){
+        
+    }
+}
--- TesteFuncao01/src/testefuncao01/TesteFuncao01.java
+++ TesteFuncao01/src/testefuncao01/TesteFuncao01.java
@@ -1,11 +1,12 @@
 package testefuncao01;
 public class TesteFuncao01 {
-    static void soma(int a, int b){
+    static int soma(int a, int b){
         int s = a + b;
-        System.out.println("A soma é " + s);
+        return s;
     }
     public static void main(String[] args) {
         System.out.println("O programa começou.");
-        soma(5,23);
+        int sm = soma(5,23);
+        System.out.println("A soma é " + sm);
     }   
 }
